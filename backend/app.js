925ffed65fa077cc51905ffac972bf12d2aa2284
Aleksandar Mitic <mitic.alex@hotmail.se>
2018-02-26 14:50:35 +0100

    Enabled cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,11 @@
 // app.js
 var express = require('express');
 var app = express();
-var db = require('./db'); 
+var cors = require('cors');
+
+app.use(cors());
 
 var Controllers = require('./controllers');
-app.use('/', Controllers);
-
-db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+app.use('/', Controllers); 
 
 module.exports = app;
